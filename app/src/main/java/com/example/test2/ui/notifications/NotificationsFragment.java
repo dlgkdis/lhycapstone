2d5a2cd07cf925e6f9ad58862518938019fb7114
dlgkdis <lhy031227@naver.com>
2024-10-07 19:02:46 +0900

    먀

--- app/src/main/java/com/example/test2/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/test2/ui/notifications/NotificationsFragment.java
@@ -10,21 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.test2.databinding.FragmentNotificationsBinding;
+import com.example.test2.databinding.FragmentDiaryBinding;
 
 public class NotificationsFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentDiaryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentDiaryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
+
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
